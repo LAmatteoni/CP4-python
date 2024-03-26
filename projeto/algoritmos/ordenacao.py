f09b6406eda95bd62e0c901fbbde7be7a359a76f
unknown <williamkhayashi@gmail.com>
2024-03-26 11:30:43 -0300

    adição do partition e quick sorte

--- projeto/algoritmos/ordenacao.py
+++ projeto/algoritmos/ordenacao.py
@@ -50,3 +50,20 @@ def merge_sort(lista):
             j += 1
             k += 1
     return lista
+
+def quick_sort(lista, low, high):
+    if low < high:
+        pi = partition(lista, low, high)
+        quick_sort(lista, low, pi-1)
+        quick_sort(lista, pi+1, high)
+    return lista
+
+def partition(lista, low, high):
+    pivot = lista[high]
+    i = low - 1
+    for j in range(low, high):
+        if lista[j] < pivot:
+            i += 1
+            lista[i], lista[j] = lista[j], lista[i]
+    lista[i+1], lista[high] = lista[high], lista[i+1]
+    return i + 1
