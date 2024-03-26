705be328f548a10750cbf428a3ea02d5e1947754
GLOnodera <gabilealonodera@gmail.com>
2024-03-26 11:27:36 -0300

    add duas ordenações

--- projeto/algoritmos/ordenacao.py
+++ projeto/algoritmos/ordenacao.py
@@ -14,3 +14,39 @@ def selection_sort(lista):
                 min_idx = j
         lista[i], lista[min_idx] = lista[min_idx], lista[i]
     return lista
+
+def insertion_sort(lista):
+    for i in range(1, len(lista)):
+        key = lista[i]
+        j = i-1
+        while j >= 0 and key < lista[j]:
+            lista[j+1] = lista[j]
+            j -= 1
+        lista[j+1] = key
+    return lista
+
+def merge_sort(lista):
+    if len(lista) > 1:
+        mid = len(lista) // 2
+        L = lista[:mid]
+        R = lista[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                lista[k] = L[i]
+                i += 1
+            else:
+                lista[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            lista[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            lista[k] = R[j]
+            j += 1
+            k += 1
+    return lista
